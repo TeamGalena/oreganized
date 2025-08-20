aae040bba92c347a9fef9f1c912b4cec857aa98c
Niklas Widmann <nik@somethingcatchy.net>
2025-08-20 16:02:28 +0200

    fix manual application jei entries for grooved ice

--- src/main/java/galena/oreganized/mixin/compat/LogStrippingFakeRecipesMixin.java
+++ src/main/java/galena/oreganized/mixin/compat/LogStrippingFakeRecipesMixin.java
@@ -13,6 +13,7 @@ import java.util.List;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -20,18 +21,18 @@ import org.spongepowered.asm.mixin.injection.At;
 public class LogStrippingFakeRecipesMixin {
 
     @ModifyReturnValue(method = "createRecipes", at = @At("RETURN"), require = 0)
-    private static List<ManualApplicationRecipe> addGroovedRecipes(List<ManualApplicationRecipe> value) {
+    private static List<RecipeHolder<ManualApplicationRecipe>> addGroovedRecipes(List<RecipeHolder<ManualApplicationRecipe>> value) {
         var newList = new ArrayList<>(value);
 
         ScribeItem.getGroovedBlocks().forEach(entry -> {
             var blockId = BuiltInRegistries.BLOCK.getKey(entry.getKey()).getPath();
-            newList.add(
-                    new ItemApplicationRecipe.Builder<>(ManualApplicationRecipe::new, Oreganized.modLoc("manual_application/" + blockId))
-                            .withItemIngredients(Ingredient.of(entry.getKey()), Ingredient.of(OItems.SCRIBE.get()))
-                            .withItemOutputs(new ProcessingOutput(new ItemStack(entry.getValue().get()), 1F))
-                            .toolNotConsumed()
-                            .build()
-            );
+            var id = Oreganized.modLoc("manual_application/" + blockId);
+            var recipe = new ItemApplicationRecipe.Builder<>(ManualApplicationRecipe::new, id)
+                    .withItemIngredients(Ingredient.of(entry.getKey()), Ingredient.of(OItems.SCRIBE.get()))
+                    .withItemOutputs(new ProcessingOutput(new ItemStack(entry.getValue().get()), 1F))
+                    .toolNotConsumed()
+                    .build();
+            newList.add(new RecipeHolder<>(id, recipe));
         });
 
         return newList;
