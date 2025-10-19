c70c0eb9d384c124f9f7dd56af169987a2871f29
Niklas Widmann <nik@somethingcatchy.net>
2025-10-19 16:16:33 +0200

    update plugin again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,6 @@ pluginManagement {
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.53")
-    id("com.possible-triangle.packwiz") version ("1.0.53")
+    id("com.possible-triangle.helper") version ("1.0.54")
+    id("com.possible-triangle.packwiz") version ("1.0.54")
 }
