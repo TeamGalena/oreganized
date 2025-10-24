c1e47084efde04d950d8ac057fb6003ba52f7a60
Niklas Widmann <nik@somethingcatchy.net>
2025-10-24 12:47:34 +0200

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,10 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenLocal()
-        maven { url = uri("https://registry.somethingcatchy.net/repository/maven-public/") }
     }
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.54")
-    id("com.possible-triangle.packwiz") version ("1.0.54")
+    id("com.possible-triangle.helper") version ("1.0.58")
+    id("com.possible-triangle.packwiz") version ("1.0.58")
 }
