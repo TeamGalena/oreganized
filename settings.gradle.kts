c139988a949e17f82248bca3eaff51d6f38c17d3
Niklas Widmann <nik@somethingcatchy.net>
2025-10-29 12:20:22 +0100

    fix missing boulders & lead door knockback

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,6 @@ pluginManagement {
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.58")
-    id("com.possible-triangle.packwiz") version ("1.0.58")
+    id("com.possible-triangle.helper") version ("1.1")
+    id("com.possible-triangle.packwiz") version ("1.1.60")
 }
--- src/main/java/galena/oreganized/data/ODamageTags.java
+++ src/main/java/galena/oreganized/data/ODamageTags.java
@@ -1,12 +1,14 @@
 package galena.oreganized.data;
 
 import galena.oreganized.Oreganized;
+import galena.oreganized.index.ODamageSources;
 import java.util.concurrent.CompletableFuture;
 import javax.annotation.Nullable;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageType;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -24,6 +26,6 @@ public class ODamageTags extends TagsProvider<DamageType> {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-      // tag(DamageTypeTags.BYPASSES_ARMOR).add(ODamageSources.LEAD_BOLT);
+      tag(DamageTypeTags.NO_KNOCKBACK).add(ODamageSources.MOLTEN_LEAD);
     }
 }
