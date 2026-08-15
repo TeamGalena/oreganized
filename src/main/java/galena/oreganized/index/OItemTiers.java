e1f17c9e0b55d9d24c371a02c005bb8a025ef994
Niklas Widmann <nik@somethingcatchy.net>
2026-08-15 12:04:00 +0200

    fix electrum tools not being diamond mining level (#200)

--- src/main/java/galena/oreganized/index/OItemTiers.java
+++ src/main/java/galena/oreganized/index/OItemTiers.java
@@ -8,6 +8,6 @@ import net.neoforged.neoforge.common.SimpleTier;
 public class OItemTiers {
 
     public static final Tier LEAD = new SimpleTier(BlockTags.INCORRECT_FOR_IRON_TOOL, 150, 7F, 1.5F, 8, () -> Ingredient.of(OTags.Items.INGOTS_LEAD));
-    public static final Tier ELECTRUM = new SimpleTier(BlockTags.INCORRECT_FOR_IRON_TOOL, 1561, 8F, 3.0F, 14, () -> Ingredient.of(OTags.Items.INGOTS_ELECTRUM));
+    public static final Tier ELECTRUM = new SimpleTier(BlockTags.INCORRECT_FOR_DIAMOND_TOOL, 1561, 8F, 3.0F, 14, () -> Ingredient.of(OTags.Items.INGOTS_ELECTRUM));
 
 }
